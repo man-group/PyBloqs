db3985d2d4b40de5314314be9d33bd504aa7e7b1
Dominik Christ <dominik.christ@ahl.com>
2017-04-13 15:17:20 +0100

    Release Version 1.0.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 Changelog
 
-### 1.0 (2017-04-07)
+### 1.0.0 (2017-04-07)
 
   * Initial public release
+
+### 1.0.1 (2017-04-13)
+
+  * Integration with pypi and ReadTheDocs.
+  * Installation on Mac and Windows.
+  
--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ class PyTest(TestCommand):
 
 setup(
     name="pybloqs",
-    version="1.0.0",
+    version="1.0.1",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description="Data Visualization and Report Building",
