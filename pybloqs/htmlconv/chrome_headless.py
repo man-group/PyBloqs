9109cb977379854068090cb9677ebe134da1a77d
Matthew Dodds <mdodds@ahl.com>
2018-04-23 15:37:37 +0100

    Use pupeteer to interract with headless chrome to expose more pdf export options

--- pybloqs/htmlconv/chrome_headless.py
+++ pybloqs/htmlconv/chrome_headless.py
@@ -1,44 +1,44 @@
 import os
 from pybloqs.htmlconv.html_converter import HTMLConverter
-import shutil
 import tempfile
 
-import pybloqs.util as util
+
+_NODE_SCRIPT_LOC = os.path.dirname(os.path.realpath(__file__)) + "/puppeteer.js"
 
 
 class ChromeHeadlessConverter(HTMLConverter):
     """
-    Implementation of HTML to PDF conversion using headless chrome.
-    """
+    Implementation of HTML to PDF conversion using headless chrome,
+    accessed via puppeteer node.js bindings.
 
+    See package.json at the root of this project for the prerequisite node.js
+    dependencies
+    """
     def htmlconv(self, block, output_file, header_block=None, header_spacing=None,
                  footer_block=None, footer_spacing=None, **kwargs):
         """
         Use headless chrome to convert the supplied html content to a PDF file.
 
-        :param input_file: File name for converter input.
+        :param block: The block to render
         :param output_file: File name for converter output.
-        :param args: Optional. List of extra command line args to pass to the converter tool.
+        :param header_block: A header block to add
+        :param header_spacing: The spacing for the header
+        :param footer_block: A footer block to add
+        :param footer_spacing: The spacing for the footer
+        :param kwargs: Optional. Named args used in page-specific css.
         """
-        # page_css = {'size': '{} {}'.format(self.page_size, self.page_orientation.lower()),
-        page_css = {'size': 'A4 landscape',
-                    'margin': 0}
-        page_css.update(kwargs)
-        content = block.render_html(static_output=True, header_block=header_block, footer_block=footer_block,
-                                    page_css=page_css)
-        name = util.str_base(abs(hash(block._id))) + ".html"
-        tempdir = tempfile.gettempdir()
-        html_filename = os.path.join(tempdir, name)
-        with open(html_filename, "w") as f:
+        zoom = kwargs.pop('zoom', .75)
+        content = block.render_html(static_output=True,
+                                    header_block=header_block,
+                                    footer_block=footer_block,
+                                    page_css=kwargs)
+
+        with tempfile.NamedTemporaryFile(suffix='.html') as f:
             f.write(content)
-
-        cmd = []
-        cmd.append(self.get_executable('google-chrome'))
-
-        # Specific headless chrome
-        cmd += ["--headless", "--print-to-pdf={}".format(output_file)]
-        [cmd.extend([k, v]) for k, v in kwargs.items()]
-        cmd.append(html_filename)
-        output, errors = self.run_command(cmd)
-
-        return output, errors
+            return self.run_command([
+                self.get_executable('node'),
+                _NODE_SCRIPT_LOC,
+                '-z', str(zoom),
+                "file://{}".format(f.name),
+                output_file
+            ])
