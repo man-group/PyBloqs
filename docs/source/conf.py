fe1bf7c8cbbd017d45f36fb8dc822f9c5f4511ba
skristof <szabo.strell.kristof@gmail.com>
2022-01-31 18:00:31 +0000

    Try to find node_modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -378,6 +378,6 @@ proc = subprocess.Popen(f"npm install highcharts", stdout=subprocess.PIPE, shell
 (out, err) = proc.communicate()
 print(f"NPM install \n{out}\nERR:{err}")
 
-proc = subprocess.Popen(f"ls -la /usr/local/lib/", stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen(f"find /usr -name node_modules", stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
-print(f"list /usr/local/: \n{out}\n{err if err else ''}")
+print(f"Find node_modules: \n{out}\n{err if err else ''}")
