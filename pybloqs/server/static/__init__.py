f4dfae23d21179e1d94f12e2d8a017f8e8b83f83
Robert Spencer <me@robertandrewspencer.com>
2025-01-23 22:45:17 +0000

    Don't use pkg_resources as it is removed in 312

--- pybloqs/server/static/__init__.py
+++ pybloqs/server/static/__init__.py
@@ -1,6 +1,5 @@
-from pkg_resources import resource_filename
-
 from pybloqs.static import JScript
+from pybloqs.util import get_resource_path
 
-with open(resource_filename(__name__, "htmx.min.js")) as f:
+with open(get_resource_path(__name__, "htmx.min.js")) as f:
     HTMX = JScript(script_string=f.read(), name="htmx")
